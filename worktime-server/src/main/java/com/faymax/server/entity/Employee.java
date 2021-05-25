0983ad7fbf2e269ce110f10bf3a6c1c0d2ba13a3
HaoPengfei <haopengfei91@126.com>
2021-05-25 23:34:08 +0800

    完成员工分页查询

--- worktime-server/src/main/java/com/faymax/server/entity/Employee.java
+++ worktime-server/src/main/java/com/faymax/server/entity/Employee.java
@@ -2,8 +2,12 @@ package com.faymax.server.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import java.time.LocalDate;
+
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -11,7 +15,7 @@ import lombok.EqualsAndHashCode;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author Faymax
@@ -19,7 +23,7 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-@ApiModel(value="Employee对象", description="")
+@ApiModel(value = "Employee对象", description = "")
 public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -35,6 +39,7 @@ public class Employee implements Serializable {
     private String gender;
 
     @ApiModelProperty(value = "出生日期")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Shanghai")
     private LocalDate birthday;
 
     @ApiModelProperty(value = "身份证号")
@@ -83,6 +88,7 @@ public class Employee implements Serializable {
     private String school;
 
     @ApiModelProperty(value = "入职日期")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Shanghai")
     private LocalDate beginDate;
 
     @ApiModelProperty(value = "在职状态")
@@ -95,19 +101,41 @@ public class Employee implements Serializable {
     private Double contractTerm;
 
     @ApiModelProperty(value = "转正日期")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Shanghai")
     private LocalDate conversionTime;
 
     @ApiModelProperty(value = "离职日期")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Shanghai")
     private LocalDate notWorkDate;
 
     @ApiModelProperty(value = "合同起始日期")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Shanghai")
     private LocalDate beginContract;
 
     @ApiModelProperty(value = "合同终止日期")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Shanghai")
     private LocalDate endContract;
 
     @ApiModelProperty(value = "工龄")
     private Integer workAge;
 
+    @ApiModelProperty(value = "民族")
+    @TableField(exist = false)
+    private Nation nation;
 
+    @ApiModelProperty(value = "政治面貌")
+    @TableField(exist = false)
+    private PoliticsStatus politicsStatus;
+
+    @ApiModelProperty(value = "部门")
+    @TableField(exist = false)
+    private Department department;
+
+    @ApiModelProperty(value = "职位")
+    @TableField(exist = false)
+    private Position position;
+
+    @ApiModelProperty(value = "职称")
+    @TableField(exist = false)
+    private JobLevel jobLevel;
 }
